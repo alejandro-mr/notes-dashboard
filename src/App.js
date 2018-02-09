d1c6f91e3c8436d16e4486e6e838946581b39fbf
Alejandro Moreno <jose.alejandro.mr@gmail.com>
2018-02-08 23:56:22 -0700

    Inital commit with project structure and Dockerfile

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, {
+  Component
+} from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Hello world on R
+      </div>
+    );
+  }
+}
+
+export default App;
