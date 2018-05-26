9030f8f182477a4e1012eb844eb3daf6d7edff71
Alejandro Moreno <jose.alejandro.mr@gmail.com>
2018-05-26 02:27:48 -0700

    Adding recompose as a dependency and updating client.js with defaults values.

--- web/src/client.js
+++ web/src/client.js
@@ -1,10 +1,11 @@
 import ApolloClient from "apollo-boost";
 
-import { resolvers } from './resolvers';
+import { defaults, resolvers } from './resolvers';
 
 const client = new ApolloClient({
   uri: "http://localhost:8080/graphql",
   clientState: {
+    defaults,
     resolvers
   }
 });
